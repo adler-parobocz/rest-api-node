48d74a5337d3de761a52796733d517a60c6c1d3a
Adler Areas Parobocz <aparobocz@gmail.com>
2019-09-25 15:03:19 -0300

    alter data connection

--- banco/dbconnection.js
+++ banco/dbconnection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const conexao = mysql.createPool({
-    host: '192.168.151.197',
-    user: 'root',
-    password: 'M3rc3d3s@17',
-    database: 'jpa'
+    host: 'host',
+    user: 'user',
+    password: 'pass',
+    database: 'database'
 });
 
 module.exports = conexao;
